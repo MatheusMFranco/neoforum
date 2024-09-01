d52573d0a107dfba5aaba40c7bf7bf2abaa22c5b
Matheus Franco <cajondefox@gmail.com>
2024-09-01 06:20:21 -0300

    fix: get jwt secret on application properties

--- src/main/kotlin/br/com/magalzim/neoforum/config/JWTUtil.kt
+++ src/main/kotlin/br/com/magalzim/neoforum/config/JWTUtil.kt
@@ -3,23 +3,29 @@ package br.com.magalzim.neoforum.config
 import br.com.magalzim.neoforum.service.UserService
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.security.Keys
+import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.stereotype.Component
 import java.nio.charset.StandardCharsets
 import java.util.*
+import javax.crypto.SecretKey
 
 @Component
 class JWTUtil(
     private val userService: UserService
 ) {
-
     @Value("\${jwt.secret}")
-    private var secret: String = "2a12Dpr9yBjZksrrC34hnQEG1uDyF5HKckz3Cob4j5md1Jl3jXPF1ejzi"
+    private lateinit var secret: String
 
     private val expiration: Long = 60000
-    private val key = Keys.hmacShaKeyFor(secret.toByteArray(StandardCharsets.UTF_8))
+    private lateinit var key: SecretKey
+
+    @PostConstruct
+    fun init() {
+        key = Keys.hmacShaKeyFor(secret.toByteArray(StandardCharsets.UTF_8))
+    }
 
     fun generateToken(username: String, authorities: MutableCollection<out GrantedAuthority>): String {
         return Jwts
